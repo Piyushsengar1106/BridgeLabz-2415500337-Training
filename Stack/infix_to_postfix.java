df708503533901fa91403ceb99ee840564ae303b
PIYUSH SENGAR <piyush.sengar_cs.aiml24@gla.ac.in>
2026-03-17 15:51:34 +0530

    add file

---
+++ Stack/infix_to_postfix.java
@@ -0,0 +1,56 @@
+ import java.util.Stack;
+public class infix_to_postfix {
+    static int precedence(char ch) {
+        switch (ch) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+            default:
+                return -1;
+        }
+    }
+
+    static String infixToPostfix(String exp) {
+        String result = "";
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < exp.length(); i++) {
+            char c = exp.charAt(i);
+            if (Character.isLetterOrDigit(c)) {
+                result += c;
+            }
+            else if (c == '(') {
+                stack.push(c);
+            }
+            else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    result += stack.pop();
+                }
+                stack.pop();
+            }
+            else {
+                while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek())) {
+                    result += stack.pop();
+                }
+                stack.push(c);
+            }
+        }
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String exp = "a+(b*c+d)";
+        System.out.println("Postfix: " + infixToPostfix(exp));
+    }
+}
+  
+
